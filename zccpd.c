4ae37f640ab03dc46d9ed9f03d5d7d4f11272375
Pieter Hintjens <ph@imatix.com>
2014-09-17 11:25:58 +0200

    Problem: need basic daemon

--- zccpd.c
+++ zccpd.c
@@ -2,7 +2,10 @@
 
 int main (void)
 {
-    zccp_server_test (true);
-    zccp_client_test (false);
+    zactor_t *server = zactor_new (zccp_server, "zccpd");
+    zsock_send (server, "s", "VERBOSE");
+    zsock_send (server, "ss", "BIND", "tcp://*:50013");
+    zsock_wait (server);
+    zactor_destroy (&server);
     return 0;
 }
